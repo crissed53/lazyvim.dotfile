eb551efc1230a370860a3f2edd18a4e989bcc031
crissed <crissed@AD02400633.local>
2024-02-12 13:21:02 +0900

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,21 @@
 return {
-  { "rebelot/kanagawa.nvim", lazy = true },
-  { "LazyVim/LazyVim", opts = { colorscheme = "kanagawa" } },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    enabled = false,
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    }
+  },
 }
