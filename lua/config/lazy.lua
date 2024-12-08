87a5c42f4750fdbec922ae67cb921744b88e388c
crissed <jisu.jeon53@navercorp.com>
2024-12-08 18:11:54 +0900

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+    { import = "lazyvim.plugins.extras.util.dot" },
+
     { import = "lazyvim.plugins.extras.ai.copilot" },
 
     -- import/override with your plugins
