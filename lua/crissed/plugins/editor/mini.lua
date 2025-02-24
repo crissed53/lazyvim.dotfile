55bc4a4276d82bf0e788c6981f050bb2a0907ae4
root <crissed53@github.com>
2025-02-25 01:29:52 +0900

    update

---
+++ lua/crissed/plugins/editor/mini.lua
@@ -0,0 +1,26 @@
+return {
+	{ "echasnovski/mini.ai", opts = {} },
+	{
+		"echasnovski/mini.surround",
+		opts = {
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
+			},
+		},
+	},
+	{
+		"echasnovski/mini.basics",
+		opts = {
+			mappings = {
+				windows = true,
+			},
+		},
+	},
+	{ "echasnovski/mini.trailspace", opts = {} },
+}
