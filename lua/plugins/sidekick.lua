dc998f6eedf163af90d23cd8633d8f9821142ba3
Chris Jeon <jisu.jeon@twelvelabs.io>
2025-11-16 15:49:39 +0900

    snack now default picker

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -81,22 +81,30 @@ return {
     },
   },
   {
-    "saghen/blink.cmp",
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
+    "folke/sidekick.nvim",
+    opts = {},
+    keys = {
+      {
+        "<tab>",
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if require("sidekick").nes_jump_or_apply() then
+            return -- jumped or applied
+          end
 
-      keymap = {
-        ["<Tab>"] = {
-          "snippet_forward",
-          function() -- sidekick next edit suggestion
-            return require("sidekick").nes_jump_or_apply()
-          end,
-          function() -- if you are using Neovim's native inline completions
-            return vim.lsp.inline_completion.get()
-          end,
-          "fallback",
-        },
+          -- if you are using Neovim's native inline completions
+          if vim.lsp.inline_completion.get() then
+            return
+          end
+
+          -- any other things (like snippets) you want to do on <tab> go here.
+
+          -- fall back to normal tab
+          return "<tab>"
+        end,
+        mode = { "i", "n" },
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
       },
     },
   },
